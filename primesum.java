a570450e2179d4d77d8196f564a78637e3dc38ef
Siddhant mishra R <47175620+RSiddhantMishra@users.noreply.github.com>
2019-06-22 15:55:44 +0530

    Add files via upload

---
+++ primesum.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+class primesum
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        List<Integer> al=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        al.add(0);
+        al.add(1);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    al.add(i);
+                    break;
+                }
+            }
+        }
+        int sum=0;
+        for(int i=0;i<al.size();i++)
+        {
+            int p=al.get(i);
+            sum+=a[p];
+        }
+        System.out.print(sum);
+    }
+}
